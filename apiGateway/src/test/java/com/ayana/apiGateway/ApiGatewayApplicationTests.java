c2069868e523be4e481f8faa8f9aef0852966c33
RohithArjun <botturohith605@gmail.com>
2026-07-02 15:52:37 +0530

    Initial Commit

---
+++ apiGateway/src/test/java/com/ayana/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayana.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ commonLibrary/commonLibrary/src/test/java/com/ayana/commonLibrary/CommonLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayana.commonLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
